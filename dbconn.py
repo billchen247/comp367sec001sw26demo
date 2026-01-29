fcc37a3324c8138feaf0664e9914a034e9481e0a
Bill Chen <bill.hongwu.chen@gmail.com>
2026-01-29 09:27:21 -0500

    fix #15 setup dbname

--- dbconn.py
+++ dbconn.py
@@ -1,4 +1,8 @@
+"""
 db connection demo configuration
 just will get a conflicts for this line change configuration 
 another change just configuration 
 not real password
+"""
+dbname = "sw26sec401demodb"
+
